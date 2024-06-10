d8e56fbb77c6d3a0c5a5553916c683d72724b5e2
zhikangxie107 <zhikangxie107@gmail.com>
2024-06-10 17:15:15 -0400

    implemented feature where if navbar tab is focused it switches to active icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import TabIcon from "../../components/TabIcon";
-
+import { ImageSourcePropType } from "react-native";
 
 const TabLayout = () => {
   return (
@@ -11,55 +11,60 @@ const TabLayout = () => {
         name="index"
         options={{
           title: "dashboard",
-          tabBarIcon: () => (
-            <TabIcon
-              source={require("../../assets/navbar/inactive/home.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => {
+            let active: ImageSourcePropType = focused
+              ? require("../../assets/navbar/active/home.png")
+              : require("../../assets/navbar/inactive/home.png");
+            return <TabIcon source={active} />;
+          },
         }}
       />
       <Tabs.Screen
         name="chores"
         options={{
           title: "chores",
-          tabBarIcon: () => (
-            <TabIcon
-              source={require("../../assets/navbar/inactive/list.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => {
+            let active: ImageSourcePropType = focused
+              ? require("../../assets/navbar/active/list.png")
+              : require("../../assets/navbar/inactive/list.png");
+            return <TabIcon source={active} />;
+          },
         }}
       />
       <Tabs.Screen
         name="shopping"
         options={{
           title: "shopping",
-          tabBarIcon: () => (
-            <TabIcon
-              source={require("../../assets/navbar/inactive/cart.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => {
+            let active: ImageSourcePropType = focused
+              ? require("../../assets/navbar/active/cart.png")
+              : require("../../assets/navbar/inactive/cart.png");
+            return <TabIcon source={active} />;
+          },
         }}
       />
       <Tabs.Screen
         name="money"
         options={{
           title: "money",
-          tabBarIcon: () => (
-            <TabIcon
-              source={require("../../assets/navbar/inactive/finance.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => {
+            let active: ImageSourcePropType = focused
+              ? require("../../assets/navbar/active/finance.png")
+              : require("../../assets/navbar/inactive/finance.png");
+            return <TabIcon source={active} />;
+          },
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "profile",
-          tabBarIcon: () => (
-            <TabIcon
-              source={require("../../assets/navbar/inactive/profile.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => {
+            let active: ImageSourcePropType = focused
+              ? require("../../assets/navbar/active/profile.png")
+              : require("../../assets/navbar/inactive/profile.png");
+            return <TabIcon source={active} />;
+          },
         }}
       />
     </Tabs>
